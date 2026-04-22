6f18933a98cb025e7cda4bafadc6e29e8d2092f5
andrqer98-boop <andrqer98@gmail.com>
2026-04-22 07:43:25 +0300

    Second commit: Add primitive visit card

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ author_name = "Андрей Шемякин" # - ЗАМЕНИТЕ ЭТУ СТРО
 print("Hello, World!")
 print(f"I am a future engineer, and my name is {author_name}.")
 print("This project is now on GitHub!")
+name = author_name
+city = "Астрахань"
+specialty = "Информационные системы и технологии"
+github_username = "andrqer98-boop"
+print (f"Я {name}, город {city}, сфера деятельности {specialty}, никнейм на гитхабе {github_username}, студент 1 курса")
